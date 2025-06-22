7fb4b094a3300ca3050966a16cf57e833b108536
George T Jose <georgeb4pc@gmail.com>
2025-06-22 09:08:02 +0530

    first commit

---
+++ AssigningValues.js
@@ -0,0 +1,10 @@
+const browserVersion = 137
+var browser = "Chrome"
+function getBrowserVersion() {
+    if (browser === "Chrome") {
+        let browserVersion = 100
+        console.log("Browser Version: " + browserVersion)  
+    }  
+    console.log("Browser Version: " + browserVersion)
+}
+getBrowserVersion()
---
+++ GtjProgram.js
@@ -0,0 +1,15 @@
+console.log("Breakout Room Assignemnt")
+console.log(firstName+" : "+typeof firstName)
+console.log(companyName+" : "+typeof companyName)
+console.log(mobileNumber+" : "+typeof mobileNumber)
+console.log(isAutomation+" : "+typeof isAutomation)
+console.log(hasPlaywright+" : "+typeof hasPlaywright)
+console.log(pi)
+var firstName = "George"
+firstName = "Abraham"
+let companyName = "GTJ Company"
+let mobileNumber = 9600311812
+let isAutomation = true
+let hasPlaywright = false
+const pi = 3.14
+
