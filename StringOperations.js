c0df4a1dbf23b3a730fb86a1f4c9e74418429865
George T Jose <georgeb4pc@gmail.com>
2025-06-22 13:06:21 +0530

    conditionals and strings

--- StringOperations.js
+++ StringOperations.js
@@ -7,9 +7,8 @@ for (let i = myName.length - 1; i >= 0; i--) {
     reverseName = reverseName + myName[i];
 }
 
-console.log(reverseName);
+console.log("Position of o : "+ myName.indexOf("o"));
 
+console.log("Character at the position 4: " + myName.charAt(4));
 
-
-
-
+console.log(`Reverse : ${reverseName}`);
